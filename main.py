7d1f4ef6d508e99174b62d2dedcfecc994c73bb4
vitalikx100 <vitalikstarigin@mail.ru>
2024-10-09 18:20:44 +0400

    first lab commit

--- main.py
+++ main.py
@@ -1,35 +1,54 @@
 import numpy as np
 import yaml
+import argparse
 
-#функция считывания данных с файла
-def read_file(file):
+
+def read_file(file: str) -> dict[str, float]:
+    '''
+     Данная функция считывает файл.
+     file принимает строковое значение, представляющее собой имя файла
+     и возвращает словарь со строковым ключом и значением типа число с плавающей точкой (float).
+    '''
     with open(file, 'r') as file:
         params = yaml.safe_load(file)
     return params
 
-#реализация расчета фукнции
-def function(x, a, b, c):
-    return np.abs(a * np.exp(1j * b * x + c))**2
+def calculate_y(params: dict[str, float]) -> None :
+    '''
+     Вычисление значения функции.
+     params принимает словарь со строковым ключом и значением с типом плавающая точка (float).
+    '''
+    x_value = params['n0']
+    y_values = []
+    while x_value <= params['nk']:
+        exponenta = np.exp(1j * params['b'] * x_value + params['c'])
+        y_value = np.abs((params['a'] * exponenta)**2)
+        y_values.append(y_value)
+        x_value += params['h']
+    with open('results.txt', 'w') as file:
+        for y_value in y_values:
+            file.write(f'y: , {y_value}\n')
 
-#считываем параметры с файла config.yaml
-#записываем в переменную params
-params = read_file('config.yaml')
+def parse_args(params: dict[str, float]) -> float:
+    '''
+     params принимает словарь со строковым ключом и значением с типом плавающая точка (float)
+     Результат работы функции возвращает значение с типом плавающая точка (float)
+    '''
+    parser = argparse.ArgumentParser(description='Вычисление значения функции')
+    parser.add_argument('n0', type=float, nargs="?")
+    parser.add_argument('h', type=float, nargs="?")
+    parser.add_argument('nk', type=float, nargs="?")
+    parser.add_argument('a', type=float, nargs="?")
+    parser.add_argument('b', type=float, nargs="?")
+    parser.add_argument('c', type=float, nargs="?")
+    args = parser.parse_args()
 
-#запись значений в переменные
-n0 = params['n0']
-h = params['h']
-nk = params['nk']
-a = params['a']
-b = params['b']
-c = params['c']
+    for key, value in args._get_kwargs():
+        if value is not None:
+            params[key] = value
+    calculate_y(params)
 
-#формируем значения x в зависимости от диапазона и шага
-x_values = np.arange(n0, nk + h, h)
-
-#Вычисление значений функции
-y_values = function(x_values, a, b, c)
-
-#Запись результатов в results.txt
-with open('results.txt', 'w') as f:
-    for x, y_val in zip(x_values, y_values):
-        f.write(f'{x}, {y_val}\n')
+if __name__ == '__main__':
+    params = read_file('config.yaml')
+    calculate_y(params)
+    params = parse_args(params)
